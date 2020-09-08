95c740f6543866b7277237f90548ed56c452d85a
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-08 08:11:49 -0600

    Add project files.

---
+++ Functions.vb
@@ -0,0 +1,16 @@
+﻿Module Functions
+
+    Sub Main()
+        Dim aLetter As String
+        aLetter = "Q"
+        Console.WriteLine(aLetter)
+        Console.ReadLine()
+    End Sub
+
+    Function Letter() As String
+        Dim someOtherLetter As String
+        someOtherLetter = "Y"
+        Return someOtherLetter
+    End Function
+
+End Module
